eb4404eeed77ba87efd3e49e692660e9acbf7f1a
Oscar Cobles <oscar.cobles@eu.equinix.com>
2022-03-07 16:40:45 +0100

    fix example

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,17 +1,25 @@
 provider "equinix" {
-  client_id     = "someID"
-  client_secret = "someSecret"
+  # client_id     = "someID"
+  # client_secret = "someSecret"
 }
 
 module "equinix_fabric_connection" {
   source = "github.com/equinix-labs/terraform-equinix-connection"
 
   # required variables
-  fabric_notification_users = ["example@equinix.com"]
+  notification_users = ["example@equinix.com"]
 
   # optional variables
   seller_profile_name      = "AWS Direct Connect"
-  seller_metro_code        = "SV"
-  seller_authorization_key = "AWS-Account-ID"
-  network_edge_id          = "NE-virtual-router-uuid"
+  seller_metro_name        = "frankfurt"
+  seller_authorization_key = "AWS-account-ID"
+  network_edge_id          = "NE-device-Uuid"
+}
+
+output "fabric_connection_id" {
+  value = module.equinix_fabric_connection.primary_connection_uuid
+}
+
+output "fabric_connection_name" {
+  value = module.equinix_fabric_connection.primary_connection_name
 }
---
+++ examples/simple/versions.tf
@@ -0,0 +1,10 @@
+terraform {  
+  required_version = ">= 0.13"
+
+  required_providers {
+    equinix = {
+      source  = "equinix/equinix"
+      version = ">= 1.4.0"
+    }
+  }
+}
